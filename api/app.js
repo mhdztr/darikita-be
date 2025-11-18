407ca15da376c3d3895bd5f93888d9cd87e37f0f
mhdztr <zakkithoriq@gmail.com>
2025-11-18 08:30:09 +0700

    Fix allowed origins

--- api/app.js
+++ api/app.js
@@ -23,6 +23,7 @@ app.use(
         "http://127.0.0.1:5500",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://darikita-app.web.app"
       ];
 
       if (allowedOrigins.indexOf(origin) !== -1) {
